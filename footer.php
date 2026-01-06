71a83d31c2ec201fe150f52a01b732bc038fdffa
choeseongwoni40-prog <choeseongwoni40@gmail.com>
2026-01-06 15:16:54 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+<footer class="site-footer">
+    <div class="site-container">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.</p>
+        <p>제작: 아백 | <a href="https://aros100.com" target="_blank" style="color: #2563eb;">https://aros100.com</a></p>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
