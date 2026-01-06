1d861b36693697d5075d2db0dea67bf7e0af2adf
choeseongwoni40-prog <choeseongwoni40@gmail.com>
2026-01-06 15:17:49 +0100

    Create single.php

---
+++ single.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main class="site-container">
+    <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class('post-item'); ?>>
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <div class="post-meta">
+                작성일: <?php echo get_the_date(); ?>
+            </div>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+    <?php endwhile; ?>
+</main>
+
+<?php get_footer(); ?>
